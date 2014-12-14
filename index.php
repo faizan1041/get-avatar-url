00c4c2a56c6f41a9636bcaa6d375c8f69a9d060e
Faizan Ali <faizan1041@gmail.com>
2014-12-14 22:07:28 +0500

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: Get Avatar URL
+Plugin URI: 
+Description: get_avatar returns image, get_avatar_url will give you the image src.
+Author: Faizan Ali
+Version: 1.6
+Author URI: http://www.faizan-ali.com/
+*/
+
